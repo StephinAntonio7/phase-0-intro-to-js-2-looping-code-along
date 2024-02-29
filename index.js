d60cf2c3c835096d1add330b757f500961ed9a49
Stephin Roberts <stephin.roberts@gmail.com>
2024-02-29 17:58:10 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(cards) {
+    let thankYouMessages = [];
+    for (let i = 0; i < cards.length; i++) {
+      thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  const cards = ["Guadalupe", "Ollie", "Aki"];
+  console.log(writeCards(cards));
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(5);
